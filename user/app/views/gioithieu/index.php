4766198ad476daa43def7a89c0f381a2c18fc70f
Tam Pham <pndtam@gmail.com>
2015-11-14 15:22:35 +0700

    fix menu left, css

--- user/app/views/gioithieu/index.php
+++ user/app/views/gioithieu/index.php
@@ -1,18 +1,25 @@
 <div class="row" style="padding-top: 15px;">
 <div class="col-md-3 category">
 	<?php if($category):?>
+	<?php
+		$i = 0;
+		$total = count($category);
+	?>
 	<?php foreach($category as $v):?>
-	<div <?php if($v->id == $content->id && $content->alias != 'linh-vuc-hoat-dong-mo-dau') echo 'class="active"'?>><a href="<?php echo url($this->baseUrl.getCol($v->category, 'alias').'/'.$v->alias.'.html')?>" title="<?php echo $v->name?>"><?php echo $v->name?></a></div>
+	<div class="<?php echo "showSub_".$i;?><?php if($v->id == $content->id && $content->alias != 'linh-vuc-hoat-dong-mo-dau') echo 'active'?>" >
+		<a href="<?php echo url($this->baseUrl.getCol($v->category, 'alias').'/'.$v->alias.'.html')?>" title="<?php echo $v->name?>"><?php echo $v->name?></a>
+	</div>
 	<?php
 		$category1 = $this->getSubMenu($v->cat_id, $v->id);
 		if($category1 && count($category1) > 0) :
 			foreach($category1 as $v1):
 	?>
-		<div class="category1 <?php if($v1->id == $content->id && $content->alias != 'linh-vuc-hoat-dong-mo-dau') echo 'active'?>"><a href="<?php echo url($this->baseUrl.getCol($v1->category, 'alias').'/'.$v1->alias.'.html')?>" title="<?php echo $v1->name?>"><?php echo $v1->name?></a></div>
+		<div class="<?php echo "showSub_".$i." sub_".$i;?> category1 <?php if($v1->id == $content->id && $content->alias != 'linh-vuc-hoat-dong-mo-dau') echo 'active'?>"><a href="<?php echo url($this->baseUrl.getCol($v1->category, 'alias').'/'.$v1->alias.'.html')?>" title="<?php echo $v1->name?>"><?php echo $v1->name?></a></div>
 	<?php
 			endforeach;
 		endif;
 	?>
+	<?php $i++;?>
 	<?php endforeach;?>
 	<?php endif;?>
 </div>
@@ -27,3 +34,16 @@
 	<?php endif;?>
 </div>
 </div>
+<script>
+	$(document).ready(function(){
+		$('.category1').hide();
+		<?php for($i = 0; $i < $total; $i++):?>
+		$('.showSub_<?php echo $i?>').mouseover(function(){
+			$('.sub_<?php echo $i?>').show();
+		});
+		$('.showSub_<?php echo $i?>').mouseout(function(){
+			$('.sub_<?php echo $i?>').hide();
+		});
+		<?php endfor;?>
+	});
+</script>

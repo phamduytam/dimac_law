acd219efc4767a838c3684a74ad14de6c3384f91
Tam Pham <pndtam@gmail.com>
2016-06-29 02:38:30 +0700

    add send mail

--- user/app/controllers/ContactController.php
+++ user/app/controllers/ContactController.php
@@ -10,7 +10,43 @@ class ContactController extends Controller
 			$model->status = 1;
 			if($model->validate()){
 				if($model->save())
+				{
+					// send mail
+					try {
+						$content = '';
+						$mail = Yii::createComponent('application.components.phpmailer.JPhpMailer');
+						$mail->IsSMTP();
+						$mail->Host = 'smtp.googlemail.com';
+						$mail->Port = '465';
+						$mail->SMTPSecure = "ssl";
+						$mail->SMTPAuth = true;
+						$mail->SMTPKeepAlive = true;
+						$mail->Mailer = "smtp";
+						$mail->CharSet = 'utf-8';
+						$mail->SMTPDebug  = 0;
+						$mail->Username = 'DimacLaw.Info@gmail.com';
+						$mail->Password = '#123456789';
+						$mail->SetFrom($model->email, $model->name);
+						$content .= '<p>Họ tên: ' . $model->name . ' </p>';
+						$content .= '<p>Email: '. $model->email .'</p>';
+						$content .= '<p>Điện thoại: '. $model->phone.'</p>';
+						$content .= '<p>Nội dung: <br></p>';
+						$content .= nl2br($model->content);
+
+						$mail->Subject = '[SEND MAIL FROM WEBSITE] ' . $model->subject;
+						$mail->AltBody = '';
+						$mail->MsgHTML($content);
+						$mail->AddAddress('ls.quoctuan@gmail.com', 'Pham Quoc Tuan');
+						$mail->Send();
+
+					} catch (phpmailerException $e) {
+						echo $e->errorMessage(); //Pretty error messages from PHPMailer
+					} catch (Exception $e) {
+						echo $e->getMessage(); //Boring error messages from anything else!
+					}
+
 					user()->setFlash('messages', 'Send successful!!');
+				}
 			}
 		}
 		$tmp_model = new StaticAR();
